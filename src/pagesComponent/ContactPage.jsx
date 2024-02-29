2656931896fc233b0c36be628107a1ac8b2b0f6c
AKBOIY <AKBOIY>
2024-02-29 19:34:23 +0100

    contact us

--- src/pagesComponent/ContactPage.jsx
+++ src/pagesComponent/ContactPage.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import "./ContactPage.css"
 
 const ContactPage = () => {
   return (
-    <div>
+    <>
+    <div className='contactbody'>
+      <div className='contactusdiv'>
+        <h1>CONTACT US</h1>
+      </div>
+      <div className='contactusinput'>
+        <div className='firstnameinput'>
+          <span>First Name*</span>
+          <input type="text" name="" id="" />
+        </div>
+        <div className='lastnameinput'> 
+        <span>Last Name*</span>
+        <input type="text" name="" id="" />
+        </div>
+      </div>
       
     </div>
+    </>
   )
 }
 
