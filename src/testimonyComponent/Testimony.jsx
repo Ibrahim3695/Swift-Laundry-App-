7048ff8a1ee27e87b680da168605daa732e4b489
ekezieprosper <japhethprosper234@gmail.com>
2024-03-06 14:14:39 +0100

    ghjju

--- src/testimonyComponent/Testimony.jsx
+++ src/testimonyComponent/Testimony.jsx
@@ -5,40 +5,17 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const Testimony = () => {
-  // const [slickSettings, setSlickSettings] = useState(null);
-    // useEffect(() => {
-      const slickSettings = {
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        infinite: true,
-        autoplay: true,
-        autoplaySpeed: 2000,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-            },
-          },
-          {
-            breakpoint: 768,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-            },
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-            },
-          },
-        ],
+        const settings = {
+        dots:true,
+        infinite:true,
+        slideesToShow:2,
+        slideToScroll:1,
+        autoplay:true,
+        speed:3000,
+        autoplaySpeed:5000,
+        cssEase:"linear"
       };
       
-    // }, []);
 
   return (
     <div className='testimonyMainDiv'>
@@ -46,43 +23,36 @@ const Testimony = () => {
         <p>What people say</p>
         <h1>Testimonies</h1>
       </header>
-      <Slider {...slickSettings}>
+      <div  className="slider">
+      {/* <Slider {...settings}> */}
         <div className='testimoniesCard1'>
           <article className='profileCircle1'>
             <img src='./dani.jpg' alt='' className='daniella' />
           </article>
         </div>
-        <div className='testimoniesCard2' >
-          <div className='profileCircle2'>
-            <img src='./dani1.jpg' alt='' className='daniella' />
-          </div>
+        <div className='testimoniesCard1'>
+          <article className='profileCircle1'>
+            <img src='./dani.jpg' alt='' className='daniella' />
+          </article>
         </div>
-        <div className='testimoniesCard3'>
-          <div className='profileCircle3'>
-            <img src='./messi;s wife.jpg' alt='' className='daniella' />
-          </div>
+        <div className='testimoniesCard1'>
+          <article className='profileCircle1'>
+            <img src='./dani.jpg' alt='' className='daniella' />
+          </article>
         </div>
-        <div className='testimoniesCard3'>
-          <div className='profileCircle3'>
-            <img src='./messi;s wife.jpg' alt='' className='daniella' />
-          </div>
+        <div className='testimoniesCard1'>
+          <article className='profileCircle1'>
+            <img src='./dani.jpg' alt='' className='daniella' />
+          </article>
         </div>
-        <div className='testimoniesCard3'>
-          <div className='profileCircle3'>
-            <img src='./messi;s wife.jpg' alt='' className='daniella' />
-          </div>
+          <div className='testimoniesCard1'>
+          <article className='profileCircle1'>
+            <img src='./dani.jpg' alt='' className='daniella' />
+          </article>
         </div>
-        <div className='testimoniesCard3'>
-          <div className='profileCircle3'>
-            <img src='./messi;s wife.jpg' alt='' className='daniella' />
-          </div>
-        </div>
-        <div className='testimoniesCard3'>
-          <div className='profileCircle3'>
-            <img src='./messi;s wife.jpg' alt='' className='daniella' />
-          </div>
-        </div>
-      </Slider>
+      {/* </Slider> */}
+      </div>
+  
     </div>
   );
 };
